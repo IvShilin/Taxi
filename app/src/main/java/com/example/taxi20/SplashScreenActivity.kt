0c27f0a6a34c6dc2f3a7c86250a9c95d5744a154
Ivan Shilin <68341349+IvShilin@users.noreply.github.com>
2021-12-24 15:54:14 +0300

    Splash screen created

--- app/src/main/java/com/example/taxi20/SplashScreenActivity.kt
+++ app/src/main/java/com/example/taxi20/SplashScreenActivity.kt
@@ -1,11 +1,26 @@
 package com.example.taxi20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.lang.Exception
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val thread = Thread {
+            try {
+                sleep(2000)
+            } catch (e: Exception) {
+                Log.i("slepp", "Sleep with exception!")
+            } finally {
+                startActivity(Intent(this, SignInActivity::class.java))
+            }
+        }.start()
     }
 }
