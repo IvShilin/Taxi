c56ec2745807346e411f8bbe715cc342041ee316
Ivan Shilin <68341349+IvShilin@users.noreply.github.com>
2021-12-24 16:45:49 +0300

    Chaged from "sign in:" to "choose mode"

--- app/src/main/java/com/example/taxi20/SplashScreenActivity.kt
+++ app/src/main/java/com/example/taxi20/SplashScreenActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.util.Log
 import java.lang.Exception
 import java.lang.Thread.sleep
-import kotlin.concurrent.thread
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
@@ -16,13 +15,13 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val thread = Thread {
+        Thread {
             try {
                 sleep(2000)
             } catch (e: Exception) {
                 Log.i("slepp", "Sleep with exception!")
             } finally {
-                startActivity(Intent(this, SignInActivity::class.java))
+                startActivity(Intent(this, ChooseModeActivity::class.java))
             }
         }.start()
     }
